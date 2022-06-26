1e917c56076fb77110f7b47fabfb6689d97cb0ef
ncuy0110 <nguyenchonuy2001@gmail.com>
2022-06-26 00:15:30 +0000

    done read and write

--- mydriver2.c
+++ mydriver2.c
@@ -5,7 +5,6 @@
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
-#include "md5.h"
 
 #include "vchar_device.h"
 
@@ -19,11 +18,6 @@ typedef struct vchar_dev {
   unsigned char * data_regs;
 } vchar_dev_t;
 
-struct user_vchar {
-  char * username;
-  uint8_t * password;
-} admin;
-
 struct _vchar_drv {
   dev_t dev_num;
   struct class *dev_class;
@@ -32,6 +26,77 @@ struct _vchar_drv {
   struct cdev *vcdev;
   unsigned int open_cnt;
 } vchar_drv;
+
+int vchar_hw_read_data(vchar_dev_t *hw, int start_reg, int num_regs, char* kbuf) {
+  int read_bytes = num_regs;
+
+  // kiem tra co quyen doc du lieu khong
+  /* if ((hw->control_regs[CONTROL_ACCESS_REG] & CTRL_READ_DATA_BIT) == DISABLE) */
+  /*   return -1; */
+
+  // kiem tra xem dia chi kbuf co hop le khong
+  if (kbuf == NULL)
+    return -1;
+
+  // kiem tra xem vi tri cua cac thanh gi can doc co hop ly khong
+  if (start_reg > NUM_DATA_REGS) 
+    return -1;
+
+  // dieu chinh lai so luong thanh ghi du lieu can doc (neu can thiet)
+  if (num_regs > (NUM_DATA_REGS - start_reg))
+    read_bytes = NUM_DATA_REGS - start_reg;
+
+  memcpy(kbuf, hw->data_regs + start_reg, read_bytes);
+
+  hw->status_regs[READ_COUNT_L_REG] +=1;
+  if (hw->status_regs[READ_COUNT_L_REG] == 0)
+    hw->status_regs[READ_COUNT_L_REG] +=1;
+
+  return read_bytes;
+}
+
+int vchar_hw_write_data(vchar_dev_t *hw, int start_reg, int num_regs, char *kbuf){
+  int write_bytes = num_regs;
+
+  /* if ((hw->control_regs[CONTROL_ACCESS_REG] & CTRL_WRITE_DATA_BIT) == DISABLE) */
+  /*   return -1; */
+
+  if(kbuf == NULL) 
+    return -1;
+
+  printk("%c\n", kbuf[0]);
+  if(start_reg > NUM_DATA_REGS)
+    return -1;
+
+  while (kbuf[0] == ' '){
+    int i = 0;
+    for (i=0; i<write_bytes-1; i++){
+      kbuf[i] = kbuf[i+1];
+    }
+
+    write_bytes-=1;
+    kbuf[write_bytes] = '\0';
+  }
+
+  while (kbuf[write_bytes-1] == ' ') {
+    write_bytes-=1;
+    kbuf[write_bytes] = '\0';
+    printk("%d\n", write_bytes);
+  }
+
+  if(num_regs > (NUM_DATA_REGS - start_reg)){
+    write_bytes = NUM_DATA_REGS - start_reg;
+    hw->status_regs[DEVICE_STATUS_REG] |= STS_DATAREGS_OVERFLOW_BIT;
+  }
+
+
+  memcpy(hw->data_regs + start_reg, kbuf, write_bytes);
+  hw->status_regs[WRITE_COUNT_L_REG] +=1;
+  if(hw->status_regs[WRITE_COUNT_L_REG] == 0)
+    hw->status_regs[WRITE_COUNT_L_REG] += 1;
+
+  return write_bytes;
+}
 
 static int vchar_driver_open(struct inode *inode, struct file *filp) {
   vchar_drv.open_cnt++;
@@ -44,14 +109,55 @@ static int vchar_driver_release(struct inode *inode, struct file *filp) {
   return 0;
 }
 
-/* static ssize_t vchar_driver_login(char *username, char *password) { */
+static ssize_t vchar_driver_read(struct file *filp, char __user *user_buf, size_t len, loff_t *off) {
+  char *kernel_buf = NULL;
+  int num_bytes = 0;
 
-/* } */
+  printk("Handle read event start from %lld, %zu bytes\n", *off, len);
+
+  kernel_buf = kzalloc(len, GFP_KERNEL);
+  if(kernel_buf == NULL)
+    return 0;
+
+  num_bytes = vchar_hw_read_data(vchar_drv.vchar_hw, *off, len, kernel_buf);
+  printk("read %d bytes from HW\n", num_bytes);
+
+  if (num_bytes < 0)
+    return -EFAULT;
+
+  if (copy_to_user(user_buf, kernel_buf, num_bytes))
+    return -EFAULT;
+
+  *off += num_bytes;
+  return num_bytes;
+}
+
+static ssize_t vchar_driver_write(struct file *filp, const char __user *user_buf, size_t len, loff_t *off) {
+  char *kernel_buf = NULL;
+
+  int num_bytes = 0;
+  printk("Handle write event start from %lld, %zu bytes\n", *off, len);
+
+  kernel_buf = kzalloc(len, GFP_KERNEL);
+  if (copy_from_user(kernel_buf, user_buf, len))
+    return -EFAULT;
+
+  num_bytes = vchar_hw_write_data(vchar_drv.vchar_hw, *off, len, kernel_buf);
+  printk("writes %d bytes to HW\n", num_bytes);
+
+  if (num_bytes < 0)
+    return -EFAULT;
+
+  *off += num_bytes;
+  return num_bytes;
+}
 
 static struct file_operations fops = {
   .owner    = THIS_MODULE,
   .open     = vchar_driver_open,
   .release  = vchar_driver_release,
+  .read     = vchar_driver_read,
+  .write    = vchar_driver_write,
 };
 
 int vchar_hw_init(vchar_dev_t *hw) {
@@ -79,9 +185,6 @@ static int __init vchar_driver_init(void) {
   /* cap phat device number */
   int ret = 0;
   vchar_drv.dev_num = 0;
-
-  admin.username = "ncuy0110";
-  admin.password = md5String("helloWorld");
 
   ret = alloc_chrdev_region(&vchar_drv.dev_num, 0, 1, "vchar_driver");
   if (ret < 0) {
--- user_app/user_test.c
+++ user_app/user_test.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
+#define BUFFER_SIZE 1024
 #define DEVICE_NODE "/dev/vchar_dev"
+
+
+/* ham kiem tra entry point release cua vchar driver */
+void close_chardev(int fd) {
+    close(fd);
+}
 
 /* ham kiem tra entry point open cua vchar driver */
 int open_chardev() {
@@ -15,10 +23,37 @@ int open_chardev() {
     return fd;
 }
 
-/* ham kiem tra entry point release cua vchar driver */
-void close_chardev(int fd) {
-    close(fd);
+void read_data_chardev() {
+  int ret = 0;
+  char user_buf[BUFFER_SIZE];
+
+  int fd = open_chardev();
+  ret = read(fd, user_buf, BUFFER_SIZE);
+  close_chardev(fd);
+
+  if (ret < 0)
+    printf("Could not read a message from %s\n", DEVICE_NODE);
+  else
+    printf("Read a message from %s: %s\n", DEVICE_NODE, user_buf);
 }
+
+void write_data_chardev() {
+  int ret = 0;
+  char user_buf[BUFFER_SIZE];
+
+  printf("Enter your message: ");
+  scanf(" %[^\n]s", user_buf);
+
+  int fd = open_chardev();
+  ret = write(fd, user_buf, strlen(user_buf) + 1);
+  close_chardev(fd);
+
+  if (ret < 0)
+    printf("Could not write a message to %s\n", DEVICE_NODE);
+  else
+    printf("Wrote the message to %s\n", DEVICE_NODE);
+}
+
 
 int main() {
     int ret = 0;
@@ -27,6 +62,8 @@ int main() {
     printf("Select below options:\n");
     printf("\to (to open a device node)\n");
     printf("\tc (to close the device node)\n");
+    printf("\tr (to read data from device node\n)");
+    printf("\tw (to write data to device node\n)");
     printf("\tq (to quit the application)\n");
     while (1) {
         printf("Enter your option: ");
@@ -45,6 +82,12 @@ int main() {
                 else
                     printf("%s has not opened yet! Can not close\n", DEVICE_NODE);
                 fd = -1;
+                break;
+            case 'r':
+                read_data_chardev();
+                break;
+            case 'w':
+                write_data_chardev();
                 break;
             case 'q':
                 if (fd > -1)
